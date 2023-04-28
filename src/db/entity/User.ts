72ac9695323b4e48365b721273b9981ad6ed2c5c
FyreFox360 <jmge5833@gmail.com>
2023-04-28 14:38:24 -0600

    Dependencias

--- src/db/entity/User.ts
+++ src/db/entity/User.ts
@@ -4,7 +4,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 export class User {
 
     @PrimaryGeneratedColumn()
-    id: number
+    id: number 
 
     @Column()
     firstName: string
