412a09afbb2068c8ba51f94448a73fb9d390e618
Fabio Flores <fabioflores@ravn.com>
2023-05-16 20:24:42 -0600

    refactor: change details user service

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -2,46 +2,48 @@ import bcrypt from 'bcryptjs';
 import boom from 'boom';
 import { User } from '../db/entity/User';
 import { AppDataSource } from '../data-source';
+import { userEntry, userEntryWithoutSensitiveInfo } from '../utils/types/user';
 
 export class UserService {
   private userRepository = AppDataSource.getRepository(User);
 
-  async create(data): Promise<User> {
+  async create(data: userEntry): Promise<userEntryWithoutSensitiveInfo> {
     const salt = bcrypt.genSaltSync(10);
-    const hashPassword = bcrypt.hashSync(data.password, salt);
-    const newUser: User = Object.assign(new User(), {
-      ...data,
+    const hashPassword: string = await bcrypt.hash(data.password, salt);
+    const birthday = new Date(data.birthday);
+    const newUser: userEntryWithoutSensitiveInfo = Object.assign(new User(), {
       password: hashPassword,
       email: data.email.toLowerCase(),
+      birthday,
+      ...data,
     });
 
     await this.userRepository.save(newUser);
-    delete newUser.password;
     return newUser;
   }
 
-  async getUser(email): Promise<User> {
-    const getuser = await this.userRepository.findOneBy({ email });
-    if (getuser === null) {
-      throw boom.badRequest('User not found');
-    } else if (email != getuser.email) {
+  async getUser(email: string): Promise<userEntry> {
+    const getuser: userEntry = await this.userRepository.findOneBy({ email });
+
+    if (!getuser) {
       throw boom.badRequest('User not found');
     }
+
     return getuser;
   }
 
   async getUserForId(id): Promise<User> {
     const getuser = await this.userRepository.findOneBy({ id });
-    if (getuser === null) {
-      throw boom.badRequest('User not found');
-    } else if (id != getuser.id) {
+
+    if (!getuser) {
       throw boom.badRequest('User not found');
     }
+
     delete getuser.password;
     return getuser;
   }
 
-  async deleteUser(email) {
+  async deleteUser(email: string) {
     const userDelete = await this.userRepository.delete({ email });
     return userDelete;
   }
@@ -53,7 +55,7 @@ export class UserService {
     return allUser;
   }
 
-  async updateUser(id, data) {
+  async updateUser(id: string, data: object) {
     await this.userRepository.update(id, data);
     //es un test para verificar que los datos se han actualizado
     //este elemento no debe existir en la etapa final
